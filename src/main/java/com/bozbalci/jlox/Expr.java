e6b09765be0a77854553dbb8c7040b2c124675e8
Berk Özbalcı <berkozbalci@gmail.com>
2019-02-01 23:16:24 +0300

    End of Chapter 8: Statements and State

--- src/main/java/com/bozbalci/jlox/Expr.java
+++ src/main/java/com/bozbalci/jlox/Expr.java
@@ -6,10 +6,27 @@ import java.util.List;
 
 abstract class Expr {
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
+  }
+
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+
+    final Token name;
+    final Expr value;
   }
 
   static class Binary extends Expr {
@@ -19,6 +36,7 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitBinaryExpr(this);
     }
@@ -33,6 +51,7 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitGroupingExpr(this);
     }
@@ -45,6 +64,7 @@ abstract class Expr {
       this.value = value;
     }
 
+    @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitLiteralExpr(this);
     }
@@ -58,12 +78,26 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitUnaryExpr(this);
     }
 
     final Token operator;
     final Expr right;
+  }
+
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
   }
 
   abstract <R> R accept(Visitor<R> visitor);
